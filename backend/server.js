c651859f5778cf52fdaba9be3677c27938001726
Aditya2846 <singhaditya2448@gmail.com>
2025-12-24 13:53:08 +0530

    Add homepage route

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,9 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 app.use("/api/tickets", ticketRoutes);
 app.use("/api/auth", authRoutes);
